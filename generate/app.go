750cce40d0d01bcf9562da9bf6017783ee3cd7e9
赵思贵 <1060656096@qq.com>
2020-04-05 12:30:51 +0800

    ccc: 修改模板

--- generate/app.go
+++ generate/app.go
@@ -19,7 +19,7 @@ func CreateApp(projectPath string, stdout io.Writer, appName string) {
 	fileName := utils.ToSnakeCase(name)
 	file := fileDir + fmt.Sprintf("/%s.go", fileName)
 
-	content := strings.Replace(apiTemplate, "{{packageName}}", packageName, -1)
+	content := strings.Replace(appTemplate, "{{packageName}}", packageName, -1)
 	content = strings.Replace(content, "{{apiName}}", name, -1)
 	utils.WriteToFile(file, content)
 	fmt.Fprintf(stdout, "\t%s%screate%s\t %s%s\n", "\x1b[32m", "\x1b[1m", "\x1b[21m", file, "\x1b[0m")
